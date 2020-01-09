12862124e65870ce1aa95e33eaa91857b28aec7c
zarkyoz <alexandre.nickles@gmail.com>
2020-01-09 11:33:46 +0100

    touche qui change de couleur au toucher

--- piano.js
+++ piano.js
@@ -63,25 +63,36 @@ btnLa.appendChild(la)
 btnSi.appendChild(si)
 btnDo2.appendChild(do2)
 
+var colorUp = "white"
+var colorDown = "red"
 
+btnDo1.addEventListener('click', function(){do1.play(); do1.currentTime = 0;})
+btnRe1.addEventListener('click', function(){re1.play(); re1.currentTime = 0 ;})
+btnMi.addEventListener('click', function(){mi.play(); mi.currentTime = 0;})
+btnFa.addEventListener('click', function(){fa.play(); fa.currentTime = 0 ;})
+btnSol.addEventListener('click', function(){sol.play(); sol.currentTime = 0 ;})
+btnLa.addEventListener('click', function(){la.play(); la.currentTime = 0 ;})
+btnSi.addEventListener('click', function(){si.play(); si.currentTime = 0 ;})
+btnDo2.addEventListener('click', function(){do2.play(); do2.currentTime = 0 ;})
 
-btnDo1.addEventListener('click', function(){do1.play()})
-btnRe1.addEventListener('click', function(){re1.play()})
-btnMi.addEventListener('click', function(){mi.play()})
-btnFa.addEventListener('click', function(){fa.play()})
-btnSol.addEventListener('click', function(){sol.play()})
-btnLa.addEventListener('click', function(){la.play()})
-btnSi.addEventListener('click', function(){si.play()})
-btnDo2.addEventListener('click', function(){do2.play()})
+btnDo1.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnRe1.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnMi.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnFa.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnSol.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnLa.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnSi.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnDo2.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
 
-btnDo1.addEventListener('mouseup', function(){do1.pause(); do1.currentTime = 0;})
-btnRe1.addEventListener('mouseup', function(){re1.pause(); re1.currentTime = 0 ;})
-btnMi.addEventListener('mouseup', function(){mi.pause(); mi.currentTime = 0 ;})
-btnFa.addEventListener('mouseup', function(){fa.pause(); fa.currentTime = 0 ;})
-btnSol.addEventListener('mouseup', function(){sol.pause(); sol.currentTime = 0 ;})
-btnLa.addEventListener('mouseup', function(){la.pause(); la.currentTime = 0 ;})
-btnSi.addEventListener('mouseup', function(){si.pause(); si.currentTime = 0 ;})
-btnDo2.addEventListener('mouseup', function(){do2.pause(); do2.currentTime = 0 ;})
+btnDo1.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnRe1.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnMi.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnFa.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnSol.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnLa.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnSi.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnDo2.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+
 
 
 window.addEventListener('keydown', function(noteMusique){
@@ -90,40 +101,86 @@ window.addEventListener('keydown', function(noteMusique){
         case "q":
             do1.play()
             do1.currentTime = 0;
+            btnDo1.style.backgroundColor = colorDown;
             break;
         case "s":
             re1.play()
             re1.currentTime = 0;
+            btnRe1.style.backgroundColor = colorDown;
         break;
         case "d":
             mi.play()
             mi.currentTime = 0;
+            btnMi.style.backgroundColor = colorDown;
         break;
         case "f":
             fa.play()
             fa.currentTime = 0;
+            btnFa.style.backgroundColor = colorDown;
         break;
         case "g":
             sol.play()
             sol.currentTime = 0;
+            btnSol.style.backgroundColor = colorDown;
         break;
         case "h":
             la.play()
             la.currentTime = 0;
+            btnLa.style.backgroundColor = colorDown;
         break;
         case "j":
             si.play()
             si.currentTime = 0;
+            btnSi.style.backgroundColor = colorDown;
         break;
         case "k":
             do2.play()
             do2.currentTime = 0;
+            btnDo2.style.backgroundColor = colorDown;
         break;
 
             default:
+              
     }
 })
 //son.currentTime=0; remet la musique a zero
+
+window.addEventListener('keyup', function(stopMusique){
+    switch (stopMusique.key){
+        
+        case "q":
+            btnDo1.style.backgroundColor = colorUp;
+            break;
+        case "s":
+            btnRe1.style.backgroundColor = colorUp;
+        break;
+        case "d":
+            btnMi.style.backgroundColor = colorUp;
+        break;
+        case "f":
+            btnFa.style.backgroundColor = colorUp;
+        break;
+        case "g":
+            btnSol.style.backgroundColor = colorUp;
+        break;
+        case "h":
+            btnLa.style.backgroundColor = colorUp;
+        break;
+        case "j":
+            btnSi.style.backgroundColor = colorUp;
+        break;
+        case "k":
+            btnDo2.style.backgroundColor = colorUp;
+        break;
+
+            
+            default:
+    }
+})
+
+
+
+
 
 // window.addEventListener('keyup', function(stopMusique){
 //     switch (stopMusique.key){
