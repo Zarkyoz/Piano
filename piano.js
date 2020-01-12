fb2870787dc759a96cad82e8a44824767abd1ded
Zarkyoz <alexandre.nickles@gmail.com>
2020-01-12 16:19:57 +0100

    ajout couleur et animation des notes

--- piano.js
+++ piano.js
@@ -1,9 +1,22 @@
 var body = document.querySelector('body');
 var header = document.createElement('header');
 var main = document.createElement('main');
+var article1 = document.createElement('article')
 
 body.appendChild(header);
 body.appendChild(main);
+header.appendChild(article1)
+
+var hr = document.createElement('hr')
+article1.appendChild(hr)
+var hr1 = document.createElement('hr')
+article1.appendChild(hr1)
+var hr2 = document.createElement('hr')
+article1.appendChild(hr2)
+var hr3 = document.createElement('hr')
+article1.appendChild(hr3)
+var hr4 = document.createElement('hr')
+article1.appendChild(hr4)
 
 var btnDo1 = document.createElement('button');
 var btnRe1 = document.createElement('button');
@@ -32,6 +45,15 @@ btnLa.textContent = "La";
 btnSi.textContent = "Si";
 btnDo2.textContent = "Do";
 
+btnDo1.id = "noteDo1"
+btnRe1.id = "noteRe1"
+btnMi.id = "noteMi"
+btnFa.id = "noteFa"
+btnSol.id = "noteSol"
+btnLa.id = "noteLa"
+btnSi.id = "noteSi"
+btnDo2.id = "noteDo2"
+
 
 main.appendChild(btnDo1)
 main.appendChild(btnRe1)
@@ -43,14 +65,14 @@ main.appendChild(btnSi)
 main.appendChild(btnDo2)
 
 
-var do1 = new Audio ("son/do1.mp3")
-var re1 = new Audio ("son/re1.mp3")
-var mi = new Audio ("son/mi.mp3")
-var fa = new Audio ("son/fa.mp3")
-var sol = new Audio ("son/sol.mp3")
-var la = new Audio ("son/la.mp3")
-var si = new Audio ("son/si.mp3")
-var do2 = new Audio ("son/do2.mp3")
+var do1 = new Audio("son/do1.mp3")
+var re1 = new Audio("son/re1.mp3")
+var mi = new Audio("son/mi.mp3")
+var fa = new Audio("son/fa.mp3")
+var sol = new Audio("son/sol.mp3")
+var la = new Audio("son/la.mp3")
+var si = new Audio("son/si.mp3")
+var do2 = new Audio("son/do2.mp3")
 
 
 
@@ -63,118 +85,226 @@ btnLa.appendChild(la)
 btnSi.appendChild(si)
 btnDo2.appendChild(do2)
 
-var colorUp = "white"
-var colorDown = "red"
 
-btnDo1.addEventListener('click', function(){do1.play(); do1.currentTime = 0; })
-btnRe1.addEventListener('click', function(){re1.play(); re1.currentTime = 0 ;})
-btnMi.addEventListener('click', function(){mi.play(); mi.currentTime = 0;})
-btnFa.addEventListener('click', function(){fa.play(); fa.currentTime = 0 ;})
-btnSol.addEventListener('click', function(){sol.play(); sol.currentTime = 0 ;})
-btnLa.addEventListener('click', function(){la.play(); la.currentTime = 0 ;})
-btnSi.addEventListener('click', function(){si.play(); si.currentTime = 0 ;})
-btnDo2.addEventListener('click', function(){do2.play(); do2.currentTime = 0 ;})
+var colorDown = "white"
+var imageX = 20;
+var imageY = 20;
+var positionDepart = 1;
+var vitesse = 50;
 
-// btnDo1.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnRe1.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnMi.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnFa.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnSol.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnLa.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnSi.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
-// btnDo2.addEventListener('mousedown', function(){this.style.backgroundColor = colorDown;})
+btnDo1.addEventListener('click', Do1)
+function Do1() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse); 
+    do1.play(); do1.currentTime = 0; var noteDo1 = new Image (imageX, imageY); noteDo1.src = "image/noteWhite.png"; noteDo1.classList = "note"; noteDo1.style.top = 73 + "%";
+    function frame(){article1.appendChild(noteDo1); noteDo1.style.left = pos + "%"; pos++;
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteDo1)
+    }
+    if (pos===80){
+        noteDo1.style.opacity = 0.3;
+    }
+}
+}
+btnRe1.addEventListener('click', Re1)
+function Re1() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    re1.play(); re1.currentTime = 0; var noteRe1 = new Image (imageX, imageY); noteRe1.src = "image/noteRed.png"; noteRe1.classList = "note"; noteRe1.style.top = 63 + "%" 
+    function frame(){article1.appendChild(noteRe1); noteRe1.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteRe1)
+    }
+    if (pos===80){
+        noteRe1.style.opacity = 0.3;
+    }
+}
+}
+btnMi.addEventListener('click', Mi)
+function Mi() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    mi.play(); mi.currentTime = 0; var noteMi = new Image (imageX, imageY); noteMi.src = "image/noteOrange.png"; noteMi.classList = "note"; noteMi.style.top = 53 + "%"
+    function frame(){article1.appendChild(noteMi); noteMi.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteMi)
+    }
+    if (pos===80){
+        noteMi.style.opacity = 0.3;
+    }
+    }
+}
+btnFa.addEventListener('click', Fa)
+function Fa() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    fa.play(); fa.currentTime = 0; var noteFa = new Image (imageX, imageY); noteFa.src = "image/noteYellow.png"; noteFa.classList = "note"; noteFa.style.top = 43 + "%"
+    function frame(){article1.appendChild(noteFa);  noteFa.style.left = pos + "%"; pos++;  
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteFa)
+    }
+    if (pos===80){
+        noteFa.style.opacity = 0.3;
+    }
+}
+}
+btnSol.addEventListener('click', Sol)
+function Sol() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    sol.play(); sol.currentTime = 0; var noteSol = new Image (imageX, imageY); noteSol.src = "image/noteGreen.png"; noteSol.classList = "note"; noteSol.style.top = 33 + "%"  
+    function frame(){article1.appendChild(noteSol);noteSol.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteSol)
+    }
+    if (pos===80){
+        noteSol.style.opacity = 0.3;
+    }
+}
+}
+btnLa.addEventListener('click', La)
+function La() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    la.play(); la.currentTime = 0; var noteLa = new Image (imageX, imageY); noteLa.src = "image/noteBlue1.png"; noteLa.classList = "note"; noteLa.style.top = 23 + "%" 
+    function frame(){article1.appendChild(noteLa); noteLa.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteLa)
+    }
+    if (pos===80){
+        noteLa.style.opacity = 0.3;
+    }
+}
+}
 
-// btnDo1.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnRe1.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnMi.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnFa.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnSol.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnLa.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnSi.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
-// btnDo2.addEventListener('mouseup', function(){this.style.backgroundColor = colorUp;})
+btnSi.addEventListener('click', Si)
+function Si() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    si.play(); si.currentTime = 0; var noteSi = new Image (imageX, imageY); noteSi.src = "image/noteBlue2.png"; noteSi.classList = "note"; noteSi.style.top = 13 + "%"  
+    function frame(){article1.appendChild(noteSi);noteSi.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteSi)
+    }
+    if (pos===80){
+        noteSi.style.opacity = 0.3;
+    }
+}
+}
+btnDo2.addEventListener('click', Do2)
+function Do2() {
+    var pos = positionDepart; var temps = setInterval(frame, vitesse);
+    do2.play(); do2.currentTime = 0; var noteDo2 = new Image (imageX, imageY); noteDo2.src = "image/notePurple.png"; noteDo2.classList = "note"; noteDo2.style.top = 3 + "%"      
+    function frame(){article1.appendChild(noteDo2);noteDo2.style.left = pos + "%"; pos++; 
+    if (pos===90){
+        clearInterval(temps); article1.removeChild(noteDo2)
+    }
+    if (pos===80){
+        noteDo2.style.opacity = 0.3;
+    }
+}
+}
 
 
 
-window.addEventListener('keydown', function(noteMusique){
-    switch (noteMusique.key){
-        
+
+
+//Partie a resoudre
+
+
+
+
+
+
+// if (window.matchMedia("(max-width: 1200px)").matches) {
+//     btnDo1.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color = "white";})
+//     btnRe1.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnMi.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnFa.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnSol.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnLa.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnSi.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+//     btnDo2.addEventListener('mousedown', function(){this.style.backgroundColor = "black"; this.style.color= "white";})
+    
+//     btnDo1.addEventListener('mouseup', function(){this.style.backgroundColor = "white"; this.style.color = "black";})
+//     btnRe1.addEventListener('mouseup', function(){this.style.backgroundColor = "red"; this.style.color = "white";})
+// btnMi.addEventListener('mouseup', function(){this.style.backgroundColor = "orange"; this.style.color = "white";})
+// btnFa.addEventListener('mouseup', function(){this.style.backgroundColor = "yellow"; this.style.color = "white";})
+// btnSol.addEventListener('mouseup', function(){this.style.backgroundColor = "green"; this.style.color = "white";})
+// btnLa.addEventListener('mouseup', function(){this.style.backgroundColor = "rgb(0, 204, 255)"; this.style.color = "white";})
+// btnSi.addEventListener('mouseup', function(){this.style.backgroundColor = "rgb(0, 0, 255)"; this.style.color = "white";})
+// btnDo2.addEventListener('mouseup', function(){this.style.backgroundColor = "purple"; this.style.color = "white";})
+// }else{
+
+// }
+
+
+
+window.addEventListener('keydown', function (noteMusique) {
+    switch (noteMusique.key) {
+
         case "q":
-            do1.play()
-            do1.currentTime = 0;
-            btnDo1.style.backgroundColor = colorDown;
+            Do1()
+            btnDo1.style.backgroundColor = "black"; btnDo1.style.color = "white"
             break;
         case "s":
-            re1.play()
-            re1.currentTime = 0;
-            btnRe1.style.backgroundColor = colorDown;
-        break;
+            Re1()
+            btnRe1.style.backgroundColor = "black"; btnRe1.style.color = "white";
+            break;
         case "d":
-            mi.play()
-            mi.currentTime = 0;
-            btnMi.style.backgroundColor = colorDown;
-        break;
+            Mi()
+            btnMi.style.backgroundColor = "black"; btnMi.style.color = "white";
+            break;
         case "f":
-            fa.play()
-            fa.currentTime = 0;
-            btnFa.style.backgroundColor = colorDown;
-        break;
+            Fa()
+            btnFa.style.backgroundColor = "black"; btnFa.style.color = "white";
+            break;
         case "g":
-            sol.play()
-            sol.currentTime = 0;
-            btnSol.style.backgroundColor = colorDown;
-        break;
+            Sol()
+            btnSol.style.backgroundColor = "black"; btnSol.style.color = "white";
+            break;
         case "h":
-            la.play()
-            la.currentTime = 0;
-            btnLa.style.backgroundColor = colorDown;
-        break;
+            La()
+            btnLa.style.backgroundColor = "black"; btnLa.style.color = "white";
+            break;
         case "j":
-            si.play()
-            si.currentTime = 0;
-            btnSi.style.backgroundColor = colorDown;
-        break;
+            Si()
+            btnSi.style.backgroundColor = "black"; btnSi.style.color = "white";
+            break;
         case "k":
-            do2.play()
-            do2.currentTime = 0;
-            btnDo2.style.backgroundColor = colorDown;
-        break;
+            Do2()
+            btnDo2.style.backgroundColor = "black"; btnDo2.style.color = "white";
+            break;
 
-            default:
-              
+        default:
+
     }
 })
 //son.currentTime=0; remet la musique a zero
 
-window.addEventListener('keyup', function(stopMusique){
-    switch (stopMusique.key){
-        
+window.addEventListener('keyup', function (stopMusique) {
+    switch (stopMusique.key) {
+
         case "q":
-            btnDo1.style.backgroundColor = colorUp;
+            btnDo1.style.backgroundColor = "white"; btnDo1.style.color = "black";
             break;
         case "s":
-            btnRe1.style.backgroundColor = colorUp;
-        break;
+            btnRe1.style.backgroundColor = "red"; btnRe1.style.color = "white";
+            break;
         case "d":
-            btnMi.style.backgroundColor = colorUp;
-        break;
+            btnMi.style.backgroundColor = "orange"; btnMi.style.color = "white";
+            break;
         case "f":
-            btnFa.style.backgroundColor = colorUp;
-        break;
-        case "g":
-            btnSol.style.backgroundColor = colorUp;
-        break;
-        case "h":
-            btnLa.style.backgroundColor = colorUp;
-        break;
+            btnFa.style.backgroundColor = "yellow"; btnFa.style.color = "white";
+            break;
+            case "g":
+                btnSol.style.backgroundColor = "green"; btnSol.style.color = "white";
+            break;
+            case "h":
+                btnLa.style.backgroundColor = "rgb(0, 204, 255)"; btnLa.style.color = "white";
+            break;
         case "j":
-            btnSi.style.backgroundColor = colorUp;
-        break;
+            btnSi.style.backgroundColor = "rgb(0, 0, 255)"; btnSi.style.color = "white";
+            break;
         case "k":
-            btnDo2.style.backgroundColor = colorUp;
-        break;
+            btnDo2.style.backgroundColor = "purple" ; btnDo2.style.color = "white";
+            break;
 
-            
-            default:
+
+        default:
     }
 })
 
@@ -184,7 +314,7 @@ window.addEventListener('keyup', function(stopMusique){
 
 // window.addEventListener('keyup', function(stopMusique){
 //     switch (stopMusique.key){
-        
+
 //         case "q":
 //             do1.pause();
 //             do1.currentTime = 0;
@@ -218,7 +348,7 @@ window.addEventListener('keyup', function(stopMusique){
 //             do2.currentTime = 0;
 //         break;
 
-            
+
 //             default:
 //     }
 // })
